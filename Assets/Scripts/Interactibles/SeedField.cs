22166393751aa9534427c11d3f6c086671ef5281
Shizounu <erik@simon-potsdam.de>
2022-05-14 16:52:25 +0200

    YEET THE CHILD

--- Assets/Scripts/Interactibles/SeedField.cs
+++ Assets/Scripts/Interactibles/SeedField.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class SeedField : interactionPoint
 {
     [Header("Stats")]
-    public bool hasSeed;
+    [SerializeField] private bool _hasSeed;
+    public bool hasSeed{
+        get {return _hasSeed;}
+        set{
+            _hasSeed = value;
+            if(_hasSeed)
+                seedVisual.SetActive(true);
+            else 
+                seedVisual.SetActive(false);
+        }
+    }
     public float totalTime = 10;
 
-    [Header("Debug Values")]
+    [Header("Debug Values/references")]
     public float growTime; //in seconds
     public GameObject saplingPrefab;
     public Transform spawnPosition;
+    public GameObject seedVisual;
+    public Animator seedAnimator;
 
-
-
+    private void OnValidate() {
+        hasSeed = _hasSeed;
+    }
 
     public override void interact(){
         if(GameController.Instance.player.holdState == HoldStateEnum.Seed){
             hasSeed = true;
+            startVisualAnim();
         }
     }
 
@@ -38,5 +52,10 @@ public class SeedField : interactionPoint
             }
         }
     }
-    
+
+
+    private void startVisualAnim(){
+        seedAnimator.SetFloat("AnimSpeed", 1 / totalTime);
+        seedAnimator.Play("Leaf Growing");
+    }   
 }
