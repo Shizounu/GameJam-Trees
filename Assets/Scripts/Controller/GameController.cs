1126c1f26cdc41188d715e0f3e4305eb34e8d40f
Shizounu <43500949+Shizounu@users.noreply.github.com>
2022-05-15 16:01:33 +0200

    added seed

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -7,9 +7,16 @@ public class GameController : MonoBehaviour
     public static GameController Instance {get; protected set;}
     public PlayerController player {get; protected set;}
 
+    [Header("Dew")]
     public Transform dewDropSpawn;
     public float dewDropTimer;
     public GameObject dewDropPrefab;
+
+    [Header("Seed")]
+    public Transform seedDropSpawn;
+    public float seedDropTimer;
+    public GameObject seedDropPrefab;
+
 
     public UnityEvent onGameOver;
 
@@ -23,20 +30,26 @@ public class GameController : MonoBehaviour
         player = FindObjectOfType<PlayerController>();
     }
 
-    public float remainingTime = 25f;
+    public float remainingTime = 45f;
 
 
     private void Start() {
         dewDropTimer = Random.Range(5,15);
+        seedDropTimer = Random.Range(5,15);
     }
 
     private void Update() {
         remainingTime -= Time.deltaTime;
         dewDropTimer -= Time.deltaTime;
+        seedDropTimer -= Time.deltaTime;
 
         if(dewDropTimer <= 0){
             dewDropTimer = Random.Range(5,15);
             Instantiate(dewDropPrefab, dewDropSpawn.position, dewDropSpawn.rotation);
+        }
+        if(seedDropTimer <= 0){
+            seedDropTimer = Random.Range(5,15);
+            Instantiate(seedDropPrefab, seedDropSpawn.position, seedDropSpawn.rotation);
         }
         if(remainingTime <= 0)
             onGameOver.Invoke();
